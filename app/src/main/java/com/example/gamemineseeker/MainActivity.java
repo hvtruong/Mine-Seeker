69bba9049153baf1b0cca226d593294f629bb1b0
Viet Truong <hvtruong@sfu.ca>
2020-06-19 21:40:20 -0700

    Initial commit, with welcome screen

---
+++ app/src/main/java/com/example/gamemineseeker/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.gamemineseeker;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Handler;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static int welcomeScreenTime = 12000;
+    private Handler handler = new Handler();
+    Animation rotate, fadeIn, fadeOut;
+    ImageView imageView;
+    TextView textView1, textView2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView2);
+        rotate = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        imageView.startAnimation(rotate);
+        Intent welcomeIntent = new Intent(MainActivity.this,MainScreen.class);
+        textView1 = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+
+        fadeIn = AnimationUtils.loadAnimation(this,R.anim.fadein);
+        textView1.startAnimation(fadeIn);
+
+        fadeOut = AnimationUtils.loadAnimation(this,R.anim.fadeout);
+        textView2.startAnimation(fadeOut);
+
+        handler.postDelayed(() -> {
+            startActivity(welcomeIntent);
+            finish();
+        }, welcomeScreenTime);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(view -> {
+            if(handler !=null){
+                handler.removeCallbacksAndMessages(null);
+            }
+            startActivity(welcomeIntent);
+            finish();
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
