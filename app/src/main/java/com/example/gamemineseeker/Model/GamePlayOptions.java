6c3ed48aa03dae60ce8ae846e9c7eb1cb9fdf4f4
Viet Truong <hvtruong@sfu.ca>
2020-06-21 06:00:37 -0700

    Done with Game Play logic, may review later

---
+++ app/src/main/java/com/example/gamemineseeker/Model/GamePlayOptions.java
@@ -0,0 +1,96 @@
+package com.example.gamemineseeker.Model;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GamePlayOptions {
+    private int numRow, numCol, numMine;
+    private static GamePlayOptions instance;
+    private ArrayList<MineCoordinate> hiddenMinesManager = new ArrayList<>();
+
+    public GamePlayOptions(){
+        numRow = 4;
+        numCol = 7;
+        numMine = 8;
+        populateHiddenMines();
+    }
+
+    public GamePlayOptions(int row, int col, int mine){
+        numRow = row;
+        numCol = col;
+        numMine = mine;
+        populateHiddenMines();
+    }
+
+    public static GamePlayOptions getInstance(){
+        if(instance == null)
+            instance = new GamePlayOptions();
+        return instance;
+    }
+
+    public int getNumMine() {
+        return numMine;
+    }
+
+    public void setNumMine(int numMine) {
+        this.numMine = numMine;
+    }
+
+    public int getNumCol() {
+        return numCol;
+    }
+
+    public void setNumCol(int numCol) {
+        this.numCol = numCol;
+    }
+
+    public int getNumRow() {
+        return numRow;
+    }
+
+    public void setNumRow(int numRow) {
+        this.numRow = numRow;
+    }
+
+    public static int generateRandomInt(int upper){
+        Random random = new Random();
+        return random.nextInt(upper);
+    }
+
+    public MineCoordinate getMine(int index){
+        return hiddenMinesManager.get(index);
+    }
+
+    public boolean isMineHere(int checkedRow, int checkedCol){
+        for(int i = 0; i <hiddenMinesManager.size(); i++){
+            if(hiddenMinesManager.get(i).getMineRow() == checkedRow && hiddenMinesManager.get(i).getMineCol() == checkedCol){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void populateHiddenMines(){
+        for(int i = 1; i <= numMine; i++ ){
+            int randomRow = generateRandomInt(numRow);
+            int randomCol = generateRandomInt(numCol);
+
+            //Check if there is a mine already here
+            if(isMineHere(randomRow, randomCol) == true){
+                i--;
+                continue;
+            }
+
+            hiddenMinesManager.add(new MineCoordinate(randomRow,randomCol));
+        }
+    }
+
+    public void eraseRevealedMine(int row, int col){
+        for(int i = 0; i <hiddenMinesManager.size(); i++){
+            if(hiddenMinesManager.get(i).getMineRow() == row && hiddenMinesManager.get(i).getMineCol() == col){
+                hiddenMinesManager.remove(i);
+                break;
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/gamemineseeker/View/gamePlay.java
+++ app/src/main/java/com/example/gamemineseeker/View/gamePlay.java
@@ -1,7 +1,15 @@
 package com.example.gamemineseeker.View;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 
+import com.example.gamemineseeker.Model.GamePlayOptions;
+import com.example.gamemineseeker.Model.MineCoordinate;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -12,32 +20,158 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.gamemineseeker.R;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class gamePlay extends AppCompatActivity {
 
-    private static final int NUM_ROW = 2;
-    private static final int NUM_COL = 3;
+    GamePlayOptions newGame = GamePlayOptions.getInstance();
+
+    private final int NUM_ROW = newGame.getNumRow();
+    private final int NUM_COL = newGame.getNumCol();
+
+    Button buttons[][] = new Button[NUM_ROW][NUM_COL];
+
+    int numFoundMine = 0;
+    int numOfScanUsed = 0;
+    TextView textView6;
+    TextView textView7;
+
+    ArrayList<MineCoordinate> revealedMineCoordinate = new ArrayList<>();
+    ArrayList<MineCoordinate> scanUpdateCoordinate = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
 
+        textView6 = findViewById(R.id.textView6);
+        textView7 = findViewById(R.id.textView7);
+
+        textView6.setText("Found 0 of " + newGame.getNumMine() + " mines.");
+        textView7.setText("# Scans used: 0");
         populateGamePlay();
     }
 
-    private void populateGamePlay(){
+    private void populateGamePlay() {
         TableLayout table = (TableLayout) findViewById(R.id.tableForGamePlay);
 
-        for(int row = 0; row < NUM_ROW; row++) {
+        for (int row = 0; row < NUM_ROW; row++) {
             TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f
+            ));
             table.addView(tableRow);
 
             for (int col = 0; col < NUM_COL; col++) {
                 Button button = new Button(this);
+
+                final int finalRow = row;
+                final int finalCol = col;
+
+                button.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f
+                ));
+
                 tableRow.addView(button);
+                button.setPadding(0,0,0,0);
+
+                button.setOnClickListener(view -> {
+                    if (newGame.isMineHere(finalRow, finalCol) == true) {
+                        displayMine(finalRow,finalCol);
+                        updateScanDisplay(finalRow, finalCol);
+
+                        revealedMineCoordinate.add(new MineCoordinate(finalRow, finalCol));
+
+                        numFoundMine++;
+                        textView6.setText("Found " + numFoundMine + " of " + newGame.getNumMine() + " mines.");
+                    } else {
+                        scanUpdateCoordinate.add(new MineCoordinate(finalRow, finalCol));
+                        scanDisplay(finalRow, finalCol);
+
+                        scanTriggerByRevealedMine(finalRow, finalCol);
+
+                        numOfScanUsed++;
+                        textView7.setText("# Scans used: " + numOfScanUsed);
+                    }
+                });
+
+                buttons[row][col] = button;
+            }
+        }
+    }
+
+    void displayMine(int row, int col){
+        Button subButton = buttons[row][col];
+
+        for(int i = 0; i < NUM_ROW;i++){
+            for(int j = 0; j <NUM_COL;j++){
+                Button tempButton = buttons[i][j];
+
+                int buttonW = tempButton.getWidth();
+                tempButton.setMinWidth(buttonW);
+                tempButton.setMaxWidth(buttonW);
+
+                int buttonH = tempButton.getHeight();
+                tempButton.setMinHeight(buttonH);
+                tempButton.setMaxHeight(buttonH);
+            }
+        }
+
+        int width = subButton.getWidth();
+        int height = subButton.getHeight();
+        Bitmap originalB = BitmapFactory.decodeResource(getResources(),R.drawable.image1);
+        Bitmap scaleB = Bitmap.createScaledBitmap(originalB, width, height, true);
+        Resources resources = getResources();
+        subButton.setBackground(new BitmapDrawable(resources, scaleB));
+
+        newGame.eraseRevealedMine(row, col);
+    }
+
+    void scanDisplay(int row, int col){
+
+        Button subButton = buttons[row][col];
+        int mineCounter = 0;
+
+        for(int j = 0; j < NUM_COL;j++){
+            if(newGame.isMineHere(row,j) == true)
+                mineCounter++;
+        }
+        for(int i = 0; i < NUM_ROW; i++){
+            if(newGame.isMineHere(i,col) == true)
+                mineCounter++;
+        }
+
+        subButton.setText("" + mineCounter);
+    }
+
+    void updateScanDisplay(int row, int col){
+        for(int i = 0; i < scanUpdateCoordinate.size(); i++) {
+            int subRow = scanUpdateCoordinate.get(i).getMineRow();
+            int subCol = scanUpdateCoordinate.get(i).getMineCol();
+
+            if (subRow == row || subCol == col) {
+                scanDisplay(subRow,subCol);
+            }
+        }
+    }
+
+    void scanTriggerByRevealedMine(int row, int col){
+        for(int i = 0; i < revealedMineCoordinate.size(); i++) {
+            int subRow = revealedMineCoordinate.get(i).getMineRow();
+            int subCol = revealedMineCoordinate.get(i).getMineCol();
+
+            if (subRow == row || subCol == col) {
+                scanDisplay(subRow,subCol);
             }
         }
     }
